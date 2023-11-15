78b1813010c944e1806a40817fb2330e59db052d
parkblo <badmuxi@gmail.com>
2023-11-15 23:41:35 +0900

    test: 단위 기능 테스트 구현

--- __tests__/RestaurantTest.js
+++ __tests__/RestaurantTest.js
@@ -1,28 +1,60 @@
-import Restaurant from '../src/models/Restaurant.js';
-import { DECEMBER_MENU_PRICE, DECEMBER_MENU_CATEGORY } from '../src/constants/DecemberMenu.js';
+import Restaurant from "../src/models/Restaurant.js";
 
-describe('Restaurant.js 테스트', () => {
-    test('makeOrderObject', () => {
+describe("Restaurant.js 테스트", () => {
+  test("makeOrderObject 정상 작동 확인", () => {
+    const R = new Restaurant();
+    const result = R.makeOrderObject("가나다-123,라라라-456");
+    expect(result).toEqual({ 가나다: 123, 라라라: 456 });
+  });
+
+  test("enterDate 정상 작동 확인", () => {
+    const R = new Restaurant();
+    R.enterDate("12");
+    expect(R.makeDateString()).toEqual("12");
+  });
+
+  test.each([[0, 32, "a"]])(
+    "enterDate는 1이상 31이하의 숫자가 아닌 경우 예외 처리한다.",
+    (date) => {
+      expect(() => {
         const R = new Restaurant();
-        const result = R.makeOrderObject('가나다-123,라라라-456');
-        expect(result).toEqual({'가나다': 123, '라라라': 456});
-    });
+        R.enterDate(date);
+      }).toThrow("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    },
+  );
 
-    test('DECEMBER_MENU_PRICE', () => {
-        const result = DECEMBER_MENU_PRICE['샴페인'];
-        expect(result).toEqual(25000);
+  test("enterOrder 정상 작동 확인", () => {
+    const R = new Restaurant();
+    expect(R.enterOrder("양송이수프-1,타파스-5")).toEqual({
+      양송이수프: 1,
+      타파스: 5,
     });
+  });
 
-    test('DECEMBER_MENU_CATEGORY', () => {
-        const result = DECEMBER_MENU_CATEGORY['샴페인'];
-        expect(result).toEqual('음료');
-    });
+  test.each([
+    [
+      "가나다-123",
+      "양송이수프-0",
+      "12-타파스",
+      "양송이수프-1,양송이수프-2",
+      "샴페인-1",
+      "양송이수프-21",
+    ],
+  ])(
+    "enterOrder는 입력 조건과 맞지 않을 경우 예외 처리한다.",
+    (order) => {
+      expect(() => {
+        const R = new Restaurant();
+        R.enterOrder(order);
+      }).toThrow("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    },
+  );
 
-    /*test('makeEventResultObject', () => {
-        let R = new Restaurant();
-        R.enterDate("3");
-        R.enterOrder("양송이수프-1");
-        const result = R.makeEventResultObject();
-        expect(result).toEqual({'가나다': 123, '라라라': 456});
-    });*/
+  test("calculatePurchaseAmount 정상 작동 확인", () => {
+    const R = new Restaurant();
+    R.enterOrder("양송이수프-1,타파스-1");
+    const Result = R.calculatePurchaseAmount();
+    expect(Result).toEqual(11500);
+  });
+  
 });
--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -4,4 +4,30 @@ describe('Validation 테스트', () => {
     test('잘못된 날짜일 경우 isWrongDate가 true를 반환한다.', () => {
         expect(Validation.isWrongDate(50)).toBe(true);
     });
+
+    test('잘못된 주문 형식일 경우 isWrongDate가 true를 반환한다.', () => {
+        expect(Validation.isWrongOrder('1-타파스')).toBe(true);
+    });
+
+    test('중복된 주문일 경우 isWrongItem이 true를 반환한다.', () => {
+        expect(Validation.isWrongItem({타파스: 1},'타파스')).toBe(true);
+    });
+
+    test('음료만 주문할 경우 checkOnlyDrinkOrder이 true를 반환한다.', () => {
+        expect(Validation.checkOnlyDrinkOrder({레드와인: 1})).toBe(true);
+    });
+
+    test.each([
+        [
+          {한강: 1},
+          {타파스: 0},
+          {샴페인: 1},
+          {양송이수프: 11, 타파스: 10}
+        ],
+      ])(
+        "잘못된 주문 객체일 경우 isWrongOrderObj이 true를 반환한다.",
+        (object) => {
+          expect(Validation.isWrongOrderObj(object)).toBe(true);
+        },
+      );
 });
